50c24ff987a177d92fad7d5454d230562ed83836
Vicente Suarez <vicentesuarez88@yahoo.com>
2017-01-07 15:53:52 -0800

    PD-6
    Added empty FlexibleHeightBarDemo project

---
+++ FlexibleHeightBarDemo/FlexibleHeightBarDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlexibleHeightBarDemo
+//
+//  Created by Vicente Suarez on 1/7/17.
+//  Copyright © 2017 Vicente Suarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
