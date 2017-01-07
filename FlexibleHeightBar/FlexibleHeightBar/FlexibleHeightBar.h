87a327dfa80a50f0606c1d04d26168fe38cce109
Vicente Suarez <vicentesuarez88@yahoo.com>
2017-01-07 15:42:34 -0800

    PD-5
    Added empty FlexibleHeightBar project

---
+++ FlexibleHeightBar/FlexibleHeightBar/FlexibleHeightBar.h
@@ -0,0 +1,19 @@
+//
+//  FlexibleHeightBar.h
+//  FlexibleHeightBar
+//
+//  Created by Vicente Suarez on 1/7/17.
+//  Copyright © 2017 Vicente Suarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlexibleHeightBar.
+FOUNDATION_EXPORT double FlexibleHeightBarVersionNumber;
+
+//! Project version string for FlexibleHeightBar.
+FOUNDATION_EXPORT const unsigned char FlexibleHeightBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlexibleHeightBar/PublicHeader.h>
+
+
